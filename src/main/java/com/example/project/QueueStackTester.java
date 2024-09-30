0179a3393d5e72060e4257ac72d5613c9fb1925f
ndem2004 <n.alhoshani7@gmail.com>
2024-09-30 16:19:53 +0300

    Queue and Stack Solution

--- src/main/java/com/example/project/QueueStackTester.java
+++ src/main/java/com/example/project/QueueStackTester.java
@@ -3,7 +3,23 @@ package com.example.project;
 public class QueueStackTester {
     public static <T> void split(Queue<T> q, Queue<T> oq, Queue<T> eq)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+    	 if(q.length() == 0)
+    		 return;
+    	 
+    	 for(int i = 1; i <= q.length(); i++) {
+    		 
+    		 T x = q.serve();
+    		 
+    		 if(i % 2 == 0)
+    			 eq.enqueue(x);
+    		 else
+    			 oq.enqueue(x);
+    		 
+    		 q.enqueue(x);
+    		 
+    	 }
+
+
         // Write the recursive static method split that splits a queue of n elements into two
         // queues. The elements with odd orders (i.e. 1st, 3rd, 5th ...) should be put in the
         // first queue and elements with even orders (i.e. 2nd, 4th, 6th ...) should be put in
@@ -13,7 +29,24 @@ public class QueueStackTester {
     }
     public static <T> void remove(LinkedPQ<T> pq, int p)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(pq.length()==0)
+     	   return;
+        
+        LinkedPQ<T> tmp= new LinkedPQ<T>();
+        int lenght =pq.length();
+        
+        for (int i = 0; i <lenght; i++) {
+     	   PQElement<T> x= pq.serve();
+     	   if(x.p >=p )
+     		   tmp.enqueue(x.data, x.p);
+ 	}
+        lenght = tmp.length();
+        
+        for (int i = 0; i < lenght; i++) {
+     	   PQElement<T> x= tmp.serve();
+     	   pq.enqueue(x.data, x.p);
+ 	}
+       
         // Write a static method remove that removes every element in the priority queue
         // having priority less than p.
         // Example. Given pq: [A, 10], [D, 8], [B, 5], [E, 3], [C, 2] remove(pq, 5) results in
@@ -21,7 +54,32 @@ public class QueueStackTester {
     }
     public static <T> boolean search(Stack<T> st, T e)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+    	boolean flag = false;
+    	
+    	if(st.empty())
+    		return flag;
+    	
+    	T arr[] = (T[]) new Object[100];
+    	int i = 0;
+    	
+    	while(!st.empty() && i < arr.length) {
+    		arr[i] = st.pop();
+    		i++;
+    	}
+    	
+    	for(int j = 0; j < i; j++) {
+    		if(arr[j] == e)
+    			flag = true;
+    	}
+    	
+    	for(i--; i > -1; i--) {
+    		st.push(arr[i]);
+    	}
+    	
+    	return flag;
+    	
+    	
+
         // Write the recursive static method search that searches for an element e in a stack st
         // and returns true if it’s found or false otherwise. st should not change at the end of
         // the method. You are not allowed to use any auxiliary data structures.
